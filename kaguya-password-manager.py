c9030f5f850300a410f6ea6a079ccc532d365137
Jordan Lee <jleejx2000@gmail.com>
2021-04-29 16:54:13 +0800

    Updated todo and added some code

---
+++ kaguya-password-manager.py
@@ -0,0 +1,37 @@
+import pyperclip as clip
+
+
+# This is the database.
+database = {}
+
+# This is the input function for a new domain.
+def kaguya_newdomain():
+    newdomain = str(input("Please enter the new domain name: "))
+    database[newdomain] = {}
+
+
+# This is the input function for a new username.
+def kaguya_newusername(domainname):
+    newusername = str(input("Please enter the new username: "))
+    newpassword = str(input("Please enter the new password: "))
+    database[domainname][newusername] = newpassword
+
+
+# This is the search function for an existing domain.
+def kaguya_existingdomain():
+    domainname = str(input('Please enter a domain name to begin the search.'))
+    if domainname in database:
+        print(f"{domainname} found in database.")
+        # If we have reached this point, we have found the domainname in the database.
+        # We now want to retrieve the username, and subsequently the password.
+    else:
+        print(f"{domainname} was not found in database.")
+        confirm_domaincreation = str(
+            input(
+                "Create new domiain in the database? (Enter Y to proceed or any other button to restart the search.)"
+            )
+        )
+        if confirm_domaincreation.lower() == "y":
+            kaguya_newdomain()
+            print('Domain has been saved into the database!')
+        else:
