ca2c599b7ca73aa13aff335c8248690be2043c78
junyi <47441677+immanuelhume@users.noreply.github.com>
2021-05-06 23:40:16 +0800

    remove old db-related code & refactor parser creation to main()

--- kaguya/cli.py
+++ kaguya/cli.py
@@ -1,6 +1,12 @@
 # Command-line Interface
 
-import argparse, string, random, re, pyperclip
+import argparse
+import random
+import re
+import string
+
+import pyperclip
+from loguru import logger
 
 # Description
 _desc = """Welcome to Kaguya Password Manager!
@@ -14,191 +20,177 @@ You may also check the strength of your passwords, or generate a strong password
 """
 
 
-# Create the parser
-kaguya_parser = argparse.ArgumentParser(
-    prog="kaguya.py",
-    description=_desc,
-    usage="%(prog)s [options]",
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-)
+def create_argparser() -> argparse.ArgumentParser:
+    kaguya_parser = argparse.ArgumentParser(
+        prog="kaguya.py",
+        description=_desc,
+        usage="%(prog)s [options]",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
 
-# Add the arguments
-kaguya_parser.add_argument(
-    "-r",
-    "--retrieve",
-    action="store_true",
-    help="activate the search and retrieve function",
-)
+    kaguya_parser.add_argument(
+        "-r",
+        "--retrieve",
+        action="store_true",
+        help="activate the search and retrieve function",
+    )
 
-kaguya_parser.add_argument(
-    "-e",
-    "--edit",
-    action="store_true",
-    help="activate the edit function to edit an existing entry",
-)
+    kaguya_parser.add_argument(
+        "-e",
+        "--edit",
+        action="store_true",
+        help="activate the edit function to edit an existing entry",
+    )
 
-kaguya_parser.add_argument(
-    "-i",
-    "--insert",
-    action="store_true",
-    help="activate the insert function to create a new entry",
-)
+    kaguya_parser.add_argument(
+        "-i",
+        "--insert",
+        action="store_true",
+        help="activate the insert function to create a new entry",
+    )
 
-kaguya_parser.add_argument(
-    "-g",
-    "--generate",
-    action="store_true",
-    help="activate the password generator",
-)
+    kaguya_parser.add_argument(
+        "-g",
+        "--generate",
+        action="store_true",
+        help="activate the password generator",
+    )
 
-kaguya_parser.add_argument(
-    "-c",
-    "--check",
-    action="store_true",
-    help="activate the password checker",
-)
+    kaguya_parser.add_argument(
+        "-c",
+        "--check",
+        action="store_true",
+        help="activate the password checker",
+    )
 
-kaguya_parser.add_argument(
-    "-d",
-    "--domain",
-    type=str,
-    help="domain that the account is used for",
-)
+    kaguya_parser.add_argument(
+        "-d",
+        "--domain",
+        type=str,
+        help="domain that the account is used for",
+    )
 
-kaguya_parser.add_argument(
-    "-u",
-    "--username",
-    type=str,
-    help="username for the specified domain",
-)
+    kaguya_parser.add_argument(
+        "-u",
+        "--username",
+        type=str,
+        help="username for the specified domain",
+    )
 
-kaguya_parser.add_argument(
-    "-p",
-    "--password",
-    type=str,
-    help="password for the specified username",
-)
+    kaguya_parser.add_argument(
+        "-p",
+        "--password",
+        type=str,
+        help="password for the specified username",
+    )
 
+    logger.debug("argparser created")
 
-# Execute the parse_args() method
-args = kaguya_parser.parse_args()
-
-
-# Argument Functions
-def retrieve(domain, username):
-    pass
+    return kaguya_parser
 
 
-def edit(domain=None, username=None, password=None):
-    while not (domain or username or password):
-        print("Please provide an argument with either the -d, -u or -p flags.")
-        break
-    # Placeholder code:
-    if domain:
-        print(domain)
-    if username:
-        print(username)
-    if password:
-        print(password)
+class HandleArgs:
+    def __init__(self, args: argparse.Namespace):
+        self.args = args
 
+        if args.retrieve:
+            logger.info("retrieve option found")
+            self.retrieve()
 
-def insert(domain=None, username=None, password=None):
-    pass
+        # TODO add logic here
+        ...
 
+    def retrieve(self, domain, username):
+        pass
 
-# The password generation function. Takes no arguments and outputs a single strong password.
-def generate():
-    """
-    Creates a password based on below criteria.
-    Checks itself to be a strong password before being returned as a value.
-    """
-    upperletters = string.ascii_uppercase
-    lowerletters = string.ascii_lowercase
-    letters = upperletters + lowerletters
-    digits = string.digits
-    symbols = "[!#$%&'()*+,-.\^_`{|}~" + r'"]'
-    allchar = upperletters + lowerletters + digits + symbols
+    def edit(self, domain=None, username=None, password=None):
+        while not (domain or username or password):
+            print("Please provide an argument with either the -d, -u or -p flags.")
+            break
+        # Placeholder code:
+        if domain:
+            print(domain)
+        if username:
+            print(username)
+        if password:
+            print(password)
 
-    length = random.randint(8, 32)
-    strong_password = [
-        random.choice(upperletters),
-        random.choice(lowerletters),
-        random.choice(digits),
-        random.choice(symbols),
-    ]
-    for i in range(length - 3):
-        strong_password.append(random.choice(allchar))
-    random.shuffle(strong_password)
-    strong_password = "".join(strong_password)
-    # Check that it is strong
-    if not check(strong_password):
-        generate()
-    else:
-        print("Password generated and copied to the clipboard.")
-        pyperclip.copy(strong_password)
+    def insert(self, domain=None, username=None, password=None):
+        pass
 
+    @staticmethod
+    def generate():
+        """
+        Creates a password based on below criteria.
+        Checks itself to be a strong password before being returned as a value.
+        """
+        upperletters = string.ascii_uppercase
+        lowerletters = string.ascii_lowercase
+        digits = string.digits
+        symbols = r"[!#$%&'()*+,-.\^_`{|}~" + r'"]'
+        allchar = upperletters + lowerletters + digits + symbols
 
-# The strong password checker. Takes the password as the only argument, outputs a boolean value on strength of password.
-def check(password):
-    """
-    Verify the strength of a 'password'.
-    Returns the strength of the password.
-    A password is considered strong if:
-        At least 8 characters in length.
-        1 digit or more.
-        1 symbol or more.
-        1 uppercase letter or more.
-        1 lowercase letter or more.
-    """
-    # Check for input
-    if password == None:
-        print("Please enter a password using the -p option.")
-        exit()
-    else:
-        # Checks the length.
-        length_error = len(password) < 8
+        length = random.randint(8, 32)
+        strong_password = [
+            random.choice(upperletters),
+            random.choice(lowerletters),
+            random.choice(digits),
+            random.choice(symbols),
+        ]
+        for i in range(length - 3):
+            strong_password.append(random.choice(allchar))
+        random.shuffle(strong_password)
+        strong_password = "".join(strong_password)
+        # Check that it is strong
+        if not HandleArgs.check(strong_password):
+            HandleArgs.generate()
+        else:
+            print("Password generated and copied to the clipboard.")
+            pyperclip.copy(strong_password)
 
-        # Checks for digit.
-        digit_error = re.search(r"\d", password) is None
+    # The strong password checker. Takes the password as the only argument,
+    #  outputs a boolean value on strength of password.
+    @staticmethod
+    def check(password) -> bool:
+        """
+        Verify the strength of a 'password'.
+        Returns the strength of the password.
+        A password is considered strong if:
+            At least 8 characters in length.
+            1 digit or more.
+            1 symbol or more.
+            1 uppercase letter or more.
+            1 lowercase letter or more.
+        """
+        # Check for input
+        if not password:
+            print("Please enter a password using the -p option.")
+            exit()
+        else:
+            # Checks the length.
+            length_error = len(password) < 8
 
-        # Checks for symbol.
-        symbol_error = re.search(r"\W", password) is None
+            # Checks for digit.
+            digit_error = re.search(r"\d", password) is None
 
-        # Checks for uppercase letter.
-        uppercase_error = re.search(r"[A-Z]", password) is None
+            # Checks for symbol.
+            symbol_error = re.search(r"\W", password) is None
 
-        # Checks for lowercase letter.
-        lowercase_error = re.search(r"[a-z]", password) is None
+            # Checks for uppercase letter.
+            uppercase_error = re.search(r"[A-Z]", password) is None
 
-        # Overall result
-        password_ok = not (
-            length_error
-            or digit_error
-            or symbol_error
-            or uppercase_error
-            or lowercase_error
-        )
+            # Checks for lowercase letter.
+            lowercase_error = re.search(r"[a-z]", password) is None
 
-        print("Password is strong.") if password_ok == True else print(
-            "Password is weak."
-        )
+            # Overall result
+            password_ok = not (
+                length_error
+                or digit_error
+                or symbol_error
+                or uppercase_error
+                or lowercase_error
+            )
 
-        return password_ok
+            print("Password is strong.") if password_ok else print("Password is weak.")
 
-
-if __name__ == "__main__":
-
-    if args.retrieve:
-        retrieve(args.domain, args.username)
-
-    if args.edit:
-        edit(args.domain, args.username, args.password)
-
-    if args.insert:
-        insert()
-
-    if args.generate:
-        generate()
-
-    if args.check:
-        check(args.password)
+            return password_ok
--- kaguya/password_manager.py
+++ kaguya/password_manager.py
@@ -1,71 +1,9 @@
-import pyperclip, argparse, os, sys, random, string, re
-
-# Test variables.
-domain = "email.com"
-user1 = "username1"
-pw1 = "password1"
-
-# This is the database.
-# placeholder
-database = {}
-
-
-class dbEntry:
-    def __init__(self, domain_name, username, password):
-        self.domain_name = domain_name
-        self.username = username
-        self.password = password
-        database.update({domain_name: {username, password}})
-
-    # This is the input method for a new username.
-    def newusername(self, domain_name, username):
-        self.newusername = username
-        database[domain_name][self.newusername] = ""
-
-    # This is the search method for an existing username.
-    def searchusername(self, domain_name, username):
-        if username in database[domain_name]:
-            print(f"{username} was found in the database.")
-            prompt_copypass = str(
-                input(
-                    "Copy password to clipboard? (Enter Y to proceed or any other button to exit the program.)"
-                )
-            )
-
-            if prompt_copypass.lower() == "y":
-                pyperclip.copy(database[domain_name][username])
-                print("Password copied to clipboard!")
-
-    # This is the input method for a new password.
-    def newpassword(self, domain_name, username, password):
-        pass
-
-    # This is the input function for a new domain.
-    def newdomain(self, domain_name):
-        self.newdomain = domain_name
-        database[self.newdomain] = {}
-        return "Domain has been created in the database!"
-
-
-# This is the search function for an existing domain.
-def searchdomain(self, domain_name):
-    if domain_name in database:
-        print(f"{domain_name} was found in the database.")
-        # If we have reached this point,
-        # we have found the domainname in the database.
-        # We now want to retrieve the username, and subsequently the password.
-        return database[domain_name]
-    # else:
-    # print(f"{domain_name} was not found in the database.")
-    # prompt_domcreate = str(
-    # input(
-    # "Create new domain in the database? (Enter Y to proceed or any other button to restart the search.)"
-    # )
-    # )
-    # if prompt_domcreate.lower() == "y":
-    # self.newdomain(domain_name)
-    # self.searchdomain(domain_name)
+from . import cli
 
 
 def main():
-    pass
+    parser = cli.create_argparser()
+    args = parser.parse_args()
+    cli.HandleArgs(args)
+
+    # TODO logic for handling args
